b7bed697f8a3d5a8b8365902db02a2a663dbe549
Hamza-Tahirr <hamzatahiramin2@gmail.com>
2024-02-25 22:43:18 +0500

    init fe

--- src/App.tsx
+++ src/App.tsx
@@ -14,20 +14,21 @@ function App() {
   return (
     <div className="flex w-screen min-h-screen bg-slate-600">
       <div className="w-1/4 p-4 max-h-screen">
-        <div className="w-full h-full border-2 border-gray-600 bg-gray-500 rounded-lg overflow-y-hidden">
+        <div className="w-full h-full border-2 border-gray-300 bg-gray-100 bg-opacity-80 rounded-lg overflow-y-hidden">
         <div className="flex items-center">
             <h1 className="mr-3 text-5xl font-semibold">ChatPDF</h1>
           </div>
-          <PDFUploadComponent onPDFUpload={handlePDFUpload} />
+          <div className='sticky bottom-0 inset-x-0 px-2 py-4'>
+          <PDFUploadComponent onPDFUpload={handlePDFUpload} /></div>
         </div>
       </div>
       <div className="w-3/5 p-4 max-h-screen">
-        <div className="w-full h-full border-2 border-gray-300 bg-gray-300 bg-opacity-80 rounded-lg overflow-y-auto">
+        <div className="w-full h-full border-2 border-gray-300 bg-gray-100 bg-opacity-80 rounded-lg overflow-y-auto">
           <PdfViewer onTextSelect={setSelectedText} />
         </div>
       </div>
       <div className="w-1/4 p-4 max-h-screen">
-        <div className="w-full h-full border-2 border-gray-300 bg-gray-300 bg-opacity-80 rounded-lg overflow-y-auto">
+        <div className="w-full h-full border-2 border-gray-300 bg-gray-100 bg-opacity-80 rounded-lg overflow-y-auto">
           <Chat selectedText={selectedText} />
         </div>
       </div>
